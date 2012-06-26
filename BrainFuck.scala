c194dae78fa970bbbc8f25381deb6d77bf2b2234
Javon Harper <javon.d.harper@gmail.com>
2012-06-26 19:32:18 -0400

    Be able to sanitize input from user

--- BrainFuck.scala
+++ BrainFuck.scala
@@ -1,12 +1,23 @@
 object Main {
+
   def main(args: Array[String]) {
     val rawInput = "thisisabrainfuck%%%%interpreterlololol>+++++++6+6+8[9<0+3+2skldfjalksdf+2ldjfald+5ldkfjdl+6+8+9+4>-]<.>+++++++[<++++>-]<+.+++++++..+++.[-]>++++++++[<++++>-]<.>+++++++++++[<++++++++>-]<-.--------.+++>.------.--------.[-]>++++++++[<++++>-]<+.[-]++++++++++."
-    new Interpreter(rawInput)
+    val bf = BrainFuck(rawInput)
   }
 }
 
-class Interpreter (rawInput: String) {
-  val program = sanitize(rawInput)
-  val validChars = "<>[]+-.,"
-  def sanitize(input: String) = { input.filter((char: Char) => validChars.contains(char)) }
+class BrainFuck (program: String) {
+}
+
+object BrainFuck {
+
+  val validChars = "+-[]<>,."
+
+  def apply(program: String) {
+    new BrainFuck(sanitize(program));
+  }
+
+  def sanitize(input: String) = { 
+    input.filter((char: Char) => validChars.contains(char))
+  }
 }
