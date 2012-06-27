715c2adb9f6203138504d652a385358ae16358ea
Javon Harper <javon.d.harper@gmail.com>
2012-06-26 20:33:22 -0400

    Implement (in|de)crement, shifting left and right, and outputting byte at pointer.

--- BrainFuck.scala
+++ BrainFuck.scala
@@ -1,29 +1,31 @@
+import java.util.Scanner
+
 object Main {
 
   def main(args: Array[String]) {
-    /*val rawInput = "thisisabrainfuck%%%%interpreterlololol>+++++++6+6+8[9<0+3+2skldfjalksdf+2ldjfald+5ldkfjdl+6+8+9+4>-]<.>+++++++[<++++>-]<+.+++++++..+++.[-]>++++++++[<++++>-]<.>+++++++++++[<++++++++>-]<-.--------.+++>.------.--------.[-]>++++++++[<++++>-]<+.[-]++++++++++."*/
-    /*val bf = BrainFuck(rawInput)*/
-    /*bf eval*/
+    val rawInput = "thisisabrainfuck%%%%interpreterlololol>+++++++6+6+8[9<0+3+2skldfjalksdf+2ldjfald+5ldkfjdl+6+8+9+4>-]<.>+++++++[<++++>-]<+.+++++++..+++.[-]>++++++++[<++++>-]<.>+++++++++++[<++++++++>-]<-.--------.+++>.------.--------.[-]>++++++++[<++++>-]<+.[-]++++++++++."
+    val bf = BrainFuck(rawInput)
+    bf eval
   }
 }
 
 class BrainFuck (program: String) {
-  val tape = new Tape
+  val tape = new Tape(program)
   val data = new DataRegister
 
   def eval {
     //DUMMY MATCH
-    'X' match {
-        case '>' => tape.increment
-        case '<' => tape.decrement
-        case '+' => data.increment
-        case '-' => data.decrement
-        case '.' => data.out
-        case ',' => data.in
-        case '[' if data.get == 0 => tape.jumpForward
-        case ']' if data.get != 0 => tape.jumpBackward
-        case _ => 
-    }
+    /*'X' match {*/
+    /*    case '>' => data.shiftRight*/
+    /*    case '<' => data.shiftLeft*/
+    /*    case '+' => data.increment*/
+    /*    case '-' => data.decrement*/
+    /*    case '.' => data.out*/
+    /*    case ',' => data.in*/
+    /*    case '[' if data.get == 0 => tape.jumpForward*/
+    /*    case ']' if data.get != 0 => tape.jumpBackward*/
+    /*    case _ => */
+    /*}*/
   }
 }
 
@@ -40,15 +42,7 @@ object BrainFuck {
   }
 }
 
-class Tape {
-  
-  def increment {
-      
-  }
-
-  def decrement {
-      
-  }
+class Tape (program: String) {
 
   def jumpForward {
       
@@ -61,24 +55,41 @@ class Tape {
 
 class DataRegister {
 
+  val data = new Array[Int](3000)
+  var pointerIndex = 0
+
   def get = {
-    //TODO implement me
-     0 
+    data.apply(pointerIndex)
+  }
+
+  def set (char: Char){
+    data.update(pointerIndex, char.toInt)
   }
 
   def increment {
-      
+    data.update(pointerIndex, get + 1)    
   }
 
   def decrement {
-      
+    data.update(pointerIndex, get - 1)    
   }
 
-  def in {
-      
+  def shiftRight {
+    if (pointerIndex < data.size) pointerIndex = pointerIndex + 1
+    else throw new Exception("Cannot increment pointer; at end of tape.")
+  }
+
+  def shiftLeft {
+    if (pointerIndex > 0) pointerIndex = pointerIndex - 1
+    else throw new Exception("Cannot decrement pointer; at beginning of tape.")
   }
 
   def out {
-      
+    print(" ");
+    print(get)
+  }
+
+  def in {
+    //TODO?
   }
 }
